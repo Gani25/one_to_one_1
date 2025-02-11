08cdd49113a1486c1e8f04d4d3846c28399ff43f
Gani25 <memonabdulgani3d@gmail.com>
2025-02-11 12:59:38 +0530

    Delete Instructor Detail and Update mapping

--- src/main/java/com/sprk/one_to_one/controller/InstructorController.java
+++ src/main/java/com/sprk/one_to_one/controller/InstructorController.java
@@ -2,6 +2,7 @@ package com.sprk.one_to_one.controller;
 
 import com.sprk.one_to_one.entity.Instructor;
 import com.sprk.one_to_one.entity.InstructorDetail;
+import com.sprk.one_to_one.repository.InstructorDetailRepository;
 import com.sprk.one_to_one.repository.InstructorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,9 @@ public class InstructorController {
 
     @Autowired
     private InstructorRepository instructorRepository;
+
+    @Autowired
+    private InstructorDetailRepository instructorDetailRepository;
 
     @PostMapping("/instructor")
     public Instructor instructor(@RequestBody Instructor instructor) {
@@ -32,8 +36,8 @@ public class InstructorController {
     }
 
     // Insert Only Instructor Detail or if instructor detail is present then update with new instructor detail
-    @PutMapping("/instructor/{instructorId}")
-    public Instructor updateInstructor(@PathVariable int instructorId, @RequestBody InstructorDetail instructorDetail) {
+    @PutMapping("/instructor/instructor_detail/{instructorId}")
+    public Instructor updateInstructorDetail(@PathVariable int instructorId, @RequestBody InstructorDetail instructorDetail) {
 
         Instructor existingInstructor = getInstructorById(instructorId);
         InstructorDetail existingInstructorDetail = existingInstructor.getInstructorDetail();
@@ -48,11 +52,52 @@ public class InstructorController {
         if(instructorDetail.getQualification() == null){
             instructorDetail.setQualification(existingInstructorDetail.getQualification());
         }
+        instructorDetail.setInstructorDetailId(existingInstructorDetail.getInstructorDetailId());
 
         existingInstructor.setInstructorDetail(instructorDetail);
 
         return instructorRepository.save(existingInstructor);
+    }
 
+    // Only update instructor object
+    @PutMapping("/instructor/{instructorId}")
+    public Instructor updateInstructor(@PathVariable int instructorId, @RequestBody Instructor instructor) {
+
+        Instructor existingInstructor = getInstructorById(instructorId);
+        InstructorDetail existingInstructorDetail = existingInstructor.getInstructorDetail();
+
+        if(instructor.getEmail() == null){
+            instructor.setEmail(existingInstructor.getEmail());
+        }
+        if(instructor.getFirstName() == null){
+            instructor.setFirstName(existingInstructor.getFirstName());
+        }
+        if(instructor.getLastName() == null){
+            instructor.setLastName(existingInstructor.getLastName());
+        }
+        if(instructor.getInstructorDetail() == null){
+            instructor.setInstructorDetail(existingInstructorDetail);
+        }
+
+        instructor.setId(existingInstructor.getId());
+
+        return instructorRepository.save(instructor);
+    }
+
+    // Delete only instructor detail
+    @DeleteMapping("/instructor/instructor_detail/{instructorId}")
+    public Instructor deleteInstructorDetail(@PathVariable int instructorId) {
+
+
+        Instructor existingInstructor = getInstructorById(instructorId);
+        InstructorDetail existingInstructorDetail = existingInstructor.getInstructorDetail();
+        existingInstructor.setInstructorDetail(null);
+        instructorDetailRepository.delete(existingInstructorDetail);
+        return instructorRepository.save(existingInstructor);
 
     }
+
+
 }
+
+
